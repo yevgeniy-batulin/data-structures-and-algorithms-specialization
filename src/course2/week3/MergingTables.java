613baaf9248fa49a79eff129a3b48273998349c5
Yevgeniy Batulin <yevgeniy.batulin@gmail.com>
2019-11-23 19:21:42 +0200

    Add week 3 tasks

---
+++ src/course2/week3/MergingTables.java
@@ -0,0 +1,145 @@
+package course2.week3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+/*
+    There are 𝑛 tables stored in some database. The tables are numbered from 1 to 𝑛.
+    All tables share the same set of columns.
+    Each table contains either several rows with real data or a symbolic link to another table.
+    Initially, all tables contain data, and 𝑖-th table has 𝑟𝑖 rows.
+    You need to perform 𝑚 of the following operations:
+        1. Consider table number 𝑑𝑒𝑠𝑡𝑖𝑛𝑎𝑡𝑖𝑜𝑛𝑖. Traverse the path of symbolic links to get to the data. That is,
+            while 𝑑𝑒𝑠𝑡𝑖𝑛𝑎𝑡𝑖𝑜𝑛𝑖 contains a symbolic link instead of real data do 𝑑𝑒𝑠𝑡𝑖𝑛𝑎𝑡𝑖𝑜𝑛𝑖 ← symlink(𝑑𝑒𝑠𝑡𝑖𝑛𝑎𝑡𝑖𝑜𝑛𝑖)
+
+        2. Consider the table number 𝑠𝑜𝑢𝑟𝑐𝑒𝑖 and traverse the path of symbolic links from it in the same manner as for 𝑑𝑒𝑠𝑡𝑖𝑛𝑎𝑡𝑖𝑜𝑛𝑖.
+
+        3. Now, 𝑑𝑒𝑠𝑡𝑖𝑛𝑎𝑡𝑖𝑜𝑛𝑖 and 𝑠𝑜𝑢𝑟𝑐𝑒𝑖 are the numbers of two tables with real data.
+        If 𝑑𝑒𝑠𝑡𝑖𝑛𝑎𝑡𝑖𝑜𝑛𝑖 ̸= 𝑠𝑜𝑢𝑟𝑐𝑒𝑖, copy all the rows from table 𝑠𝑜𝑢𝑟𝑐𝑒𝑖 to table 𝑑𝑒𝑠𝑡𝑖𝑛𝑎𝑡𝑖𝑜𝑛𝑖,
+        then clear the table 𝑠𝑜𝑢𝑟𝑐𝑒𝑖 and instead of real data put a symbolic link to 𝑑𝑒𝑠𝑡𝑖𝑛𝑎𝑡𝑖𝑜𝑛𝑖 into it.
+
+        4. Print the maximum size among all 𝑛 tables (recall that size is the number of rows in the table).
+        If the table contains only a symbolic link, its size is considered to be 0.
+ */
+public class MergingTables {
+    private final InputReader reader;
+    private final OutputWriter writer;
+    private int maximumNumberOfRows = -1;
+
+    private MergingTables(InputReader reader, OutputWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    static class Table {
+        Table parent;
+        int rank;
+        int numberOfRows;
+
+        Table(int numberOfRows) {
+            this.numberOfRows = numberOfRows;
+            rank = 0;
+            parent = this;
+        }
+
+        Table getParent() {
+            if (parent != this) {
+                parent = parent.getParent();
+            }
+            return parent;
+        }
+    }
+
+    private void merge(Table destination, Table source) {
+        Table realDestination = destination.getParent();
+        Table realSource = source.getParent();
+        if (realDestination == realSource) {
+            return;
+        }
+
+        if (realSource.rank > realDestination.rank) {
+            realDestination.parent = realSource;
+            realSource.numberOfRows += realDestination.numberOfRows;
+            realDestination.numberOfRows = 0;
+            if (realSource.numberOfRows > maximumNumberOfRows)
+                maximumNumberOfRows = realSource.numberOfRows;
+        } else {
+            realSource.parent = realDestination;
+            realDestination.numberOfRows += realSource.numberOfRows;
+            realSource.numberOfRows = 0;
+            if (realSource.rank == realDestination.rank) {
+                realDestination.rank++;
+            }
+            if (realDestination.numberOfRows > maximumNumberOfRows)
+                maximumNumberOfRows = realDestination.numberOfRows;
+        }
+    }
+
+    private void run() {
+        int n = reader.nextInt();
+        int m = reader.nextInt();
+        Table[] tables = new Table[n];
+        for (int i = 0; i < n; i++) {
+            int numberOfRows = reader.nextInt();
+            tables[i] = new Table(numberOfRows);
+            maximumNumberOfRows = Math.max(maximumNumberOfRows, numberOfRows);
+        }
+        for (int i = 0; i < m; i++) {
+            int destination = reader.nextInt() - 1;
+            int source = reader.nextInt() - 1;
+            merge(tables[destination], tables[source]);
+            writer.printf(maximumNumberOfRows);
+        }
+    }
+
+    public static void main(String[] args) {
+        InputReader reader = new InputReader(System.in);
+        OutputWriter writer = new OutputWriter(System.out);
+        new MergingTables(reader, writer).run();
+        writer.writer.flush();
+    }
+
+    static class InputReader {
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+
+        InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        OutputWriter(OutputStream stream) {
+            writer = new PrintWriter(stream);
+        }
+
+        void printf(Object... args) {
+            writer.print(String.format(Locale.ENGLISH, "%d\n", args));
+        }
+    }
+}
