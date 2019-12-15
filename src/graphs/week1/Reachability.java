40fcd95fb4b275ea12ade38a683fb6556143af09
Yevgeniy Batulin <yevgeniy.batulin@gmail.com>
2019-12-16 00:05:53 +0200

    Add first task of second week of graphs course.

--- src/graphs/week1/Reachability.java
+++ src/graphs/week1/Reachability.java
@@ -1,4 +1,4 @@
-package graphs;
+package graphs.week1;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
---
+++ src/graphs/week2/Acyclicity.java
@@ -0,0 +1,47 @@
+package graphs.week2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Acyclicity {
+    private static boolean foundCycle(List<List<Integer>> adj, int current, boolean[] visited, boolean[] visitedDuringRecursion) {
+        if (visitedDuringRecursion[current]) return true;
+
+        visited[current] = true;
+        visitedDuringRecursion[current] = true;
+
+        for (Integer neighbour : adj.get(current)) {
+            if (foundCycle(adj, neighbour, visited, visitedDuringRecursion)) return true;
+        }
+
+        visitedDuringRecursion[current] = false;
+        return false;
+    }
+
+    private static int acyclic(List<List<Integer>> adj) {
+        boolean[] visited = new boolean[adj.size()];
+        boolean[] visitedDuringRecursion = new boolean[adj.size()];
+        for (int i = 0; i < adj.size(); i++) {
+            if (!visited[i] && foundCycle(adj, i, visited, visitedDuringRecursion)) return 1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        List<List<Integer>> adj = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            int x, y;
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            adj.get(x - 1).add(y - 1);
+        }
+        System.out.println(acyclic(adj));
+    }
+}
